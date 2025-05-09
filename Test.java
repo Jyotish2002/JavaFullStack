b0d19cc5b09cfbc56f8c82b97aca7d2f2fa6f435
Jyotish <jyotishyadav09@gmail.com>
2025-05-10 02:01:28 +0530

    Java Basics

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test{
+    
+    public static void main(String[] args) {
+       
+       int a=1;
+       System.out.println(Integer.toString(a));
+
+       String name ="Jyotish yadav";
+       System.out.println(name);
+
+       for (int i = 0; i < 10; i++) {
+           System.out.println("Jyotish");
+       }
+       int i=0;
+        while(++i<10){
+            System.out.println(i);
+            
+        }
+    }
+
+    
+}
+
